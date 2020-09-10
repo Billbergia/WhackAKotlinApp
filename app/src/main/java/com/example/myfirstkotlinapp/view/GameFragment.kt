63db23998bc27cbf99a6f59c783a83dfcdf2f958
Sara Billberg <sara.billberg@visma.com>
2020-09-10 15:23:27 +0200

    Initial commit

---
+++ app/src/main/java/com/example/myfirstkotlinapp/view/GameFragment.kt
@@ -0,0 +1,32 @@
+package com.example.myfirstkotlinapp.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import com.example.myfirstkotlinapp.R
+import com.example.myfirstkotlinapp.viewmodel.GameViewModel
+
+/**
+ * A simple [Fragment] subclass as the default destination in the navigation.
+ */
+class GameFragment : Fragment() {
+
+    lateinit var viewModel: GameViewModel
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_first, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel = ViewModelProvider(this).get(GameViewModel::class.java)
+        view.findViewById<Button>(R.id.button_first).setOnClickListener {
+            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/myfirstkotlinapp/viewmodel/GameViewModel.kt
@@ -0,0 +1,16 @@
+package com.example.myfirstkotlinapp.viewmodel
+
+import androidx.lifecycle.ViewModel
+
+class GameViewModel : ViewModel() {
+
+    private val _
+
+    override fun onCleared() {
+        /*
+         This method will be called when this ViewModel is no longer used and will be destroyed.
+         It’s useful when the ViewModel observes some data, and you need to clear this subscription to prevent a leak of this ViewModel.
+         */
+        super.onCleared()
+    }
+}
